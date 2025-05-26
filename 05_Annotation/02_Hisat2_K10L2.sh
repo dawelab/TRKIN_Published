00c3808e4ef412d266da411d8cb27a5a61ba288a
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-26 15:19:11 -0400

    Update 02_Hisat2_K10L2.sh

--- 05_Annotation/02_Hisat2_K10L2.sh
+++ 05_Annotation/02_Hisat2_K10L2.sh
@@ -1,28 +1,18 @@
-#!/bin/bash
-#SBATCH --job-name=Hisat2_K10L2
-#SBATCH --output Hisat2_K10L2.%A-%a.out
-#SBATCH --partition=batch
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=mjb51923@uga.edu
-#SBATCH --ntasks=24
-#SBATCH --mem=100gb
-#SBATCH --time=24:00:00
-#SBATCH --array=1-5
-
 #Load Modules needed 
 module load HISAT2/3n-20201216-gompi-2022a
 module load SAMtools/1.17-GCC-12.2.0
 
 #Set the file name 
 THREADS=24
-READDIR=/scratch/mjb51923/TRKIN_CRISPR/out_paper/Trimmed_RNAseq_K10L2
-OUT=/scratch/mjb51923/TRKIN_CRISPR/out_paper
+#K10L2 RNAseq reads come from doi: 10.1101/gad.340679.120
+READDIR=path/to//RNAseq_K10L2
+OUT=""
 
 #Make the output directory and enter it
 #mkdir $OUT/Hisat2
 cd $OUT/K10L2_Hisat2
 
-#List of all RNA seq identifiers (one per pair) from EBI E-MTAB-8641 made in List.txt
+#List of all RNA seq identifiers (one per pair) from NCBI BioProject PRJNA285341 made in List.txt
 
 #This pulls info from the array job
 IT=$SLURM_ARRAY_TASK_ID
@@ -34,8 +24,11 @@ REF=$OUT/RepeatMasker/CI66_K10L2_v1.fasta.masked
 #Build a hisat2 reference
 #I ran this command separatly first to avoid having the reference re built every time
 #hisat2-build $REF CI66_K10L2_v1.fasta.masked
+#Align RNAseq reads to the masked reference
 hisat2 -x $OUT/K10L2_Hisat2/CI66_K10L2_v1.fasta.masked -p $THREADS -1 $READDIR/${NAME}_R1_paired.fq -2 $READDIR/${NAME}_R2_paired.fq -S $OUT/K10L2_Hisat2/${NAME}.sam
+#Convert the sam file output by hisat2 to a bam
 samtools view -bS $OUT/K10L2_Hisat2/${NAME}.sam > $OUT/K10L2_Hisat2/${NAME}.bam
+#sort the bam file
 samtools sort $OUT/K10L2_Hisat2/${NAME}.bam -o $OUT/K10L2_Hisat2/${NAME}.s.bam
 
 
