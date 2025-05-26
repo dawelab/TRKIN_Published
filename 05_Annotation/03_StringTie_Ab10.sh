f154ba4bdf825c9058f08ff7bddcd4b164ca8563
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-26 15:23:56 -0400

    Update 03_StringTie_Ab10.sh

--- 05_Annotation/03_StringTie_Ab10.sh
+++ 05_Annotation/03_StringTie_Ab10.sh
@@ -5,14 +5,15 @@ module load SAMtools/1.17-GCC-12.2.0
 #Define the variables
 OUTDIR=""
 #mkdir $OUTDIR/Stringtie_Ab10
-BAMDIR=/Hisat2
+BAMDIR=$OUTDIR/Hisat2
 
 #Merge all the bam files 
 BAM_LIST=$(ls $BAMDIR/*.s.bam)
 
 #Merge all the bam files together
 samtools merge -o $OUTDIR/Stringtie_Ab10/B73Ab10_AllTissues.bam $BAM_LIST
+#Sort the merged bam file
 samtools sort $OUTDIR/Stringtie_Ab10/B73Ab10_AllTissues.bam -o $OUTDIR/Stringtie_Ab10/B73Ab10_AllTissues.s.bam
 
-#Run the genome guided assembly
+#Run the genome guided transcriptome assembly
 stringtie -o $OUTDIR/Stringtie_Ab10/B73Ab10_Stringtie_Assembly.gtf $OUTDIR/Stringtie_Ab10/B73Ab10_AllTissues.s.bam
