7178be2003bc1d78352bc3e1b17d28546f6992e6
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-26 16:50:14 -0400

    Update 2_Knob180_Process_Repeat_Counts.R

--- 03_Estimate_Ab10_Knob_Size/2_Knob180_Process_Repeat_Counts.R
+++ 03_Estimate_Ab10_Knob_Size/2_Knob180_Process_Repeat_Counts.R
@@ -24,7 +24,7 @@ K$length <- abs(K$qend-K$qstart)
 
 AB10_filt <- subset(AB10, length >=30)
 
-#y values are average coverage of the genome determined by samtools
+#y/b/d values are average coverage of the genome determined by samtools
 
 #Ab10 
 x=sum(AB10_filt$length)
