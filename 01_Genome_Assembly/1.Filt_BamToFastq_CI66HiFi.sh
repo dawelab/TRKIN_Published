3254501d7cbe0629205717a43012ead67d017374
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-26 16:18:07 -0400

    Update 1.Filt_BamToFastq_CI66HiFi.sh

--- 01_Genome_Assembly/1.Filt_BamToFastq_CI66HiFi.sh
+++ 01_Genome_Assembly/1.Filt_BamToFastq_CI66HiFi.sh
@@ -1,20 +1,12 @@
-#!/usr/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Filt_BamToFastq_CI66HiFi
-#SBATCH --output Filt_BamToFastq_CI66HiFi.out
-#SBATCH --mem=50GB
-#SBATCH --time=5:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=1
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=BEGIN,END
-
+#Load modules
 module load BEDTools/2.30.0-GCC-12.2.0
-
 module load BamTools/2.5.2-GCC-11.2.0
 
-cd /scratch/mjb51923/raw_reads/CI66_HiFi
+#enter directory with CI66 Raw Reads located on NCBI SRA under bioproject PRJNA1254310
+cd /path/to/CI66_HiFi_Reads 
 
+#Filter the reads
 bamtools filter -in m84082_240109_064404_s1.hifi_reads.bc2079.bam -out m84082_240109_064404_s1.hifi_reads.bc2079.rq99.bam -tag "rq":">=0.99" 
 
+#Convert the bam file to a fastq
 bedtools bamtofastq -i m84082_240109_064404_s1.hifi_reads.bc2079.rq99.bam -fq CI66.HiFi.rq99.fastq
