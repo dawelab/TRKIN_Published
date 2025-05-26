b309406dcfc8cfc871b66e4b922cb247f7a19a85
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-26 16:34:57 -0400

    Update 6. BLAST_CI66_K10L2.sh

--- 01_Genome_Assembly/6. BLAST_CI66_K10L2.sh
+++ 01_Genome_Assembly/6. BLAST_CI66_K10L2.sh
@@ -1,19 +1,8 @@
-#!/bin/bash
-#SBATCH --job-name=BLAST_CI66_K10L2
-#SBATCH --output=BLAST_CI66_K10L2.out
-#SBATCH --partition=batch
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=mjb51923@uga.edu
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=30
-#SBATCH --mem=5gb
-#SBATCH --time=5:00:00
-
 #load the modules necessary from the cluster
 module load BLAST+/2.13.0-gompi-2022a
 
 #Define Variables, variables in all caps 
-DIR="/scratch/mjb51923/TRKIN_CRISPR/out_paper"
+DIR=""
 REF=$DIR/RepeatMasker/CI66_K10L2_v1.fasta.masked
 
 #make blast database
@@ -28,9 +17,6 @@ awk '{print $2, $9, $10, $3}' $DIR/BLAST/BLAST_knob180_v_CI66_K10L2.out > $DIR/B
 
 blastn -num_threads 30 -task "blastn" -outfmt 6 -db $REF -query /scratch/mjb51923/annotations/trkin_cds_1.fa -out $DIR/BLAST/BLAST_trkin_v_CI66_K10L2.out
 awk '{print $2, $9, $10, $3}' $DIR/BLAST/BLAST_trkin_v_CI66_K10L2.out > $DIR/BLAST/BLAST_trkin_v_CI66_K10L2.bed
-
-blastn -num_threads 30 -task "blastn" -outfmt 6 -db $REF -query /scratch/mjb51923/annotations/Zm00001eb434490_T001.fasta -out $DIR/BLAST/BLAST_sr2_v_CI66_K10L2.out
-awk '{print $2, $9, $10, $3}' $DIR/BLAST/BLAST_sr2_v_CI66_K10L2.out > $DIR/BLAST/BLAST_sr2_v_CI66_K10L2.bed
 
 blastn -num_threads 30 -task "blastn" -outfmt 6 -db $REF -query /scratch/mjb51923/annotations/Zm00001eb429330.fasta -out $DIR/BLAST/BLAST_r1_v_CI66_K10L2.out
 awk '{print $2, $9, $10, $3}' $DIR/BLAST/BLAST_r1_v_CI66_K10L2.out > $DIR/BLAST/BLAST_r1_v_CI66_K10L2.bed
