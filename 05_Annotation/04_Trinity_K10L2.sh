d6cb9b2242be14006f6e0af00eab457d985e46fe
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-26 15:31:33 -0400

    Update 04_Trinity_K10L2.sh

--- 05_Annotation/04_Trinity_K10L2.sh
+++ 05_Annotation/04_Trinity_K10L2.sh
@@ -1,21 +1,10 @@
-#!/bin/bash
-#SBATCH --job-name=Trinity_K10L2
-#SBATCH --output=Trinity_K10L2.out
-#SBATCH --partition=batch
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=mjb51923@uga.edu
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=200gb
-#SBATCH --time=48:00:00
-
 #Load the modules
 module load Trinity/2.15.1-foss-2022a
 
 #Define the variables
-OUTDIR=/scratch/mjb51923/TRKIN_CRISPR/out_paper
+OUTDIR=""
 mkdir $OUTDIR/Trinity_Denovo_K10L2
-READDIR=/scratch/mjb51923/TRKIN_CRISPR/out_paper/Trimmed_RNAseq_K10L2
+READDIR=/path/to/RNAseq_K10L2
 
 #List the fastq files 
 R1_LIST=$(ls $READDIR/*_R1_paired.fq)
@@ -30,5 +19,6 @@ for i in "K10L2.AllReps"
 do
 echo "########### Starting new line"
 mkdir $OUTDIR/Trinity_Denovo_K10L2/trinity.$i
+#Perform denovo transcriptome assembly using all available RNAseq data
 Trinity --seqType fq --output $OUTDIR/Trinity_Denovo_K10L2/trinity.$i --max_memory 170G --left $OUTDIR/Trinity_Denovo_K10L2/${i}_R1.fq --right $OUTDIR/Trinity_Denovo_K10L2/${i}_R2.fq --CPU 24
 done
