fdcdefd7fae935761157db8ea31f6c446230aa5b
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-26 16:19:17 -0400

    Update 2.Hifiasm_CI66HiFi.sh

--- 01_Genome_Assembly/2.Hifiasm_CI66HiFi.sh
+++ 01_Genome_Assembly/2.Hifiasm_CI66HiFi.sh
@@ -1,18 +1,8 @@
-#!/usr/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Hifiasm_CI66HiFi
-#SBATCH --output Hifiasm_CI66HiFi.out
-#SBATCH --mem=300GB
-#SBATCH --time=96:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=20
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=BEGIN,END
-
+#Load the modile
 module load hifiasm/0.19.6-GCCcore-11.3.0
 
-OUT_DIR="/scratch/mjb51923/CI66_Assembly/out"
-READS="/scratch/mjb51923/raw_reads/CI66_HiFi/CI66.HiFi.rq99.fastq"
+OUTDIR=""
+READS="/path/to/rawreads/CI66.HiFi.rq99.fastq"
 
 cd $OUT_DIR
 
