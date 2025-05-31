c854c19391a0f44a8ef7c2614517d567edc16265
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 19:16:44 -0400

    Update 10_AGAT_Ab10.sh

--- 05_Annotation/10_AGAT_Ab10.sh
+++ 05_Annotation/10_AGAT_Ab10.sh
@@ -6,7 +6,7 @@ OUTDIR=""
 DIR=$OUTDIR/AGAT
 
 #Extract the CDS sequences
-agat_sp_extract_sequences.pl -g $OUTDIR/Liftoff/Ab10_HiFi_v2_corrected.gene.v2.sorted.gff3 -f $OUTDIR/RepeatMasker/Ab10_HiFi_v2_corrected.fa.masked -t cds -o $DIR/HiFiAb10.v2.cds.fasta
+agat_sp_extract_sequences.pl -g $OUTDIR/Liftoff/B73_Ab10_HiFi_v2.gene.sorted.gff3 -f $OUTDIR/RepeatMasker/B73_Ab10_HiFi_v2.fa.masked -t cds -o $DIR/B73_Ab10_HiFi_v2.cds.fasta
 
 #Extract the mRNA sequence 
-agat_sp_extract_sequences.pl -g $OUTDIR/Liftoff/Ab10_HiFi_v2_corrected.gene.v2.sorted.gff3 -f $OUTDIR/RepeatMasker/Ab10_HiFi_v2_corrected.fa.masked -t exon -merge -o $DIR/HiFiAb10.v2.mRNA.fasta
+agat_sp_extract_sequences.pl -g $OUTDIR/Liftoff/B73_Ab10_HiFi_v2.gene.sorted.gff3 -f $OUTDIR/RepeatMasker/B73_Ab10_HiFi_v2.fa.masked -t exon -merge -o $DIR/B73_Ab10_HiFi_v2.mRNA.fasta
