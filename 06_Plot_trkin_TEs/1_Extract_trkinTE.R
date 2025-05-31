ebaef30e8244e24e80c6ad6d5a18722845bf7ccb
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 18:38:18 -0400

    Update and rename Extract_trkinTE.R to 1_Extract_trkinTE.R

--- 06_Plot_trkin_TEs/1_Extract_trkinTE.R
+++ 06_Plot_trkin_TEs/1_Extract_trkinTE.R
@@ -1,34 +1,40 @@
 library(tidyverse)
 
-setwd("/Users/user/University_of_Georgia/Dawe_Lab_Documents/Trkin_CRISPR/R Sessions/Paper/TRKIN_Paper")
+setwd("")
 
-Ab10_TE <- read.delim("~/University_of_Georgia/Dawe_Lab_Documents/Trkin_CRISPR/R Sessions/OrthoFinder/Ab10_HiFi_v2_corrected.TE.gff", header=FALSE)
+#Read in the Ab10 TE annotation
+Ab10_TE <- read.delim("B73_Ab10_HiFi_v2.TE.gff", header=FALSE)
 colnames(Ab10_TE) <- c("seqnames", "method", "repeattype", "start", "end", "something1", "strand", "dot", "description")
 
+#Isolate TEs within Ab10 trkin 1 
 Ab10TE_trkin1 <- subset(Ab10_TE, seqnames == "chr10" & start >= 148968414 & end <= 149079834 )
+#Separate out the superfamily identity
 Ab10TE_trkin1 <- separate(Ab10TE_trkin1, description, into=c("ID", "Motif"), sep=" ")
 Ab10TE_trkin1$Motif <- gsub("Motif:", "", Ab10TE_trkin1$Motif)
-Uniq <- unique(Ab10TE_trkin1$Motif)
 
+#write the Ab10 trkin1 TE annotation
 write.csv(Ab10TE_trkin1, "Ab10_trkin1_TE.csv", row.names = FALSE, quote = FALSE)
 
-
+#Isolate TEs within Ab10 trkin 2
 Ab10TE_trkin2 <- subset(Ab10_TE, seqnames == "chr10" & start >= 150358418 & end <= 150457939)
+#Separate out the superfamily identity
 Ab10TE_trkin2 <- separate(Ab10TE_trkin2, description, into=c("ID", "Motif"), sep=" ")
 Ab10TE_trkin2$Motif <- gsub("Motif:", "", Ab10TE_trkin2$Motif)
-Uniq <- unique(Ab10TE_trkin2$Motif)
 
+#write the Ab10 trkin2 TE annotation
 write.csv(Ab10TE_trkin2, "Ab10_trkin2_TE.csv", row.names = FALSE, quote = FALSE)
 
-
-K10L2_TE <- read.delim("~/University_of_Georgia/Dawe_Lab_Documents/Trkin_CRISPR/R Sessions/OrthoFinder/CI66_K10L2_v1.TE.gff", header=FALSE)
+#Read in the K10L2 TE annotation
+K10L2_TE <- read.delim("CI66_K10L2_v1.TE.gff", header=FALSE)
 colnames(K10L2_TE) <- c("seqnames", "method", "repeattype", "start", "end", "something1", "strand", "dot", "description")
 
-
+#Isolate TEs within K10l2 trkin
 K10L2TE_trkin <- subset(K10L2_TE, seqnames == "K10L2" & start >= 16280429 & end <= 16357330)
+#Separate out the superfamily identity
 K10L2TE_trkin <- separate(K10L2TE_trkin, description, into=c("ID", "Motif"), sep=" ")
 K10L2TE_trkin$Motif <- gsub("Motif:", "", K10L2TE_trkin$Motif)
-Uniq <- unique(K10L2TE_trkin$Motif)
 
+
+#write the K10L2 trkin TE annotation
 write.csv(K10L2TE_trkin, "K10L2_trkin_TE.csv", row.names = FALSE, col.names = TRUE, quote = FALSE)
 
