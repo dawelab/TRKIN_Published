5f10f8799faa6d541dfa4ffe51317f81232e0a44
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 19:20:15 -0400

    Update 12_EnTAP_Ab10.sh

--- 05_Annotation/12_EnTAP_Ab10.sh
+++ 05_Annotation/12_EnTAP_Ab10.sh
@@ -8,12 +8,12 @@ DIR=$OUTDIR/Entap
 
 #Do the basic EnTAP configuration
 cd $DIR/Ab10/
-#EnTAP --config --ini /home/mjb51923/entap_config.ini
+#EnTAP --config --ini /path/to/entap_config.ini
 
 #Configure my databases to be diamond databases
 #To get EnTAP running you need to download these databases and then run the following commands. Refer to here for instructions https://entap.readthedocs.io/en/latest/Getting_Started/Configuration/configuration.html
 
-EnTAP --config --ini path/to/entap_config.ini -i $OUTDIR/AGAT/HiFiAb10.v2.cds.fasta -d $OUTDIR/Entap/uniprot_sprot.fasta -d $OUTDIR/Entap/RefSeq.plant.protein.faa -d $OUTDIR//Entap/nr.fasta --overwrite
+EnTAP --config --ini /path/to/entap_config.ini -i $OUTDIR/AGAT/B73_Ab10_HiFi_v2.cds.fasta -d $OUTDIR/Entap/uniprot_sprot.fasta -d $OUTDIR/Entap/RefSeq.plant.protein.faa -d $OUTDIR//Entap/nr.fasta --overwrite
 
 #This runs entap using the diamond databases from the above step. runP means it will do frame selection, convert to protein, and then use blastp against the databases as I am providing a CDS transcriptome
-EnTAP --runP --ini /path/to/entap_config.ini -i $OUTDIR/AGAT/HiFiAb10.v2.cds.fasta -d $OUTDIR/Entap/K10L2/entap_outfiles/bin/uniprot_sprot.dmnd -d /$OUTDIR/Entap/K10L2/entap_outfiles/bin/RefSeq.dmnd -d $OUTDIR/Entap/K10L2/entap_outfiles/bin/nr.dmnd  --out-dir /$OUTDIR/Entap/Ab10/entap_outfiles_v2 -t 24
+EnTAP --runP --ini /path/to/entap_config.ini -i $OUTDIR/AGAT/B73_Ab10_HiFi_v2.cds.fasta -d $OUTDIR/Entap/K10L2/entap_outfiles/bin/uniprot_sprot.dmnd -d /$OUTDIR/Entap/K10L2/entap_outfiles/bin/RefSeq.dmnd -d $OUTDIR/Entap/K10L2/entap_outfiles/bin/nr.dmnd  --out-dir /$OUTDIR/Entap/Ab10/entap_outfiles_v2 -t 24
