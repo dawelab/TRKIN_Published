168de455c09c5a6a81c13f286212df9d8b10be50
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 19:08:40 -0400

    Update 08_Liftoff_K10L2.sh

--- 05_Annotation/08_Liftoff_K10L2.sh
+++ 05_Annotation/08_Liftoff_K10L2.sh
@@ -7,23 +7,27 @@ OUTDIR=""
 DIR="$OUTDIR/Liftoff"
 cd $DIR
 
-#This subsets the K10L2 and Ab10 genomes to their respective trkin specific regions (The region between the first two TR-1 knobs
-samtools faidx /scratch/mjb51923/ref_genomes/Ab10_HiFi_v2_corrected.fa chr10:142472000-153145000 > $DIR/Ab10_HiFi_v2_corrected.trkinreg.fa
-samtools faidx /scratch/mjb51923/TRKIN_CRISPR/out_paper/RepeatMasker/CI66_K10L2_v1.fasta K10L2:7429619-25502283 > $DIR/CI66_K10L2_v1.trkinreg.fa
+#Define reference genomes
+K10L2=/path/to/CI66_K10L2_v1.fasta
+Ab10=/path/to/B73_Ab10_HiFi_v2.fa
+
+#This subsets the K10L2 genome to the trkin specific regions (The region between the two TR-1 knobs)
+samtools faidx $K10L2 K10L2:7429619-25502283 > $DIR/CI66_K10L2_v1.trkinreg.fa
 
 #I manually edited the K10L2 trkin region file to name the region K10L2 instead of K10L2:7429619-25502283
 
-Ab10=/scratch/mjb51923/ref_genomes/Ab10_HiFi_v2_corrected.fa
-K10L2=/scratch/mjb51923/TRKIN_CRISPR/out_paper/RepeatMasker/CI66_K10L2_v1.fasta
+#This isolates the trkin region of each gene annotation file
+bedtools intersect -a $OUTDIR/Pasa_K10L2/PASA_updates.2.gff3 -b DIR/CI66_K10L2_v1.trkinreg.fa -o $DIR/Pasa_K10L2/PASA_updates.3.trkinreg.gff3
 
 #Lift over the annotations from the original Ab10 genome to the HiFi genome 
-liftoff -a 0.4 -s 0.4 -copies -sc 0.7 -flank 0.1 -g $DIR/pasa_db_Ab10.gene_structures_post_PASA_updates.280839.trkinreg.gff3 -o $DIR/pasa_db_K10L2.gene_structures_post_PASA_updates.3815155.trkinreg.Ab10Liftoff.gff3 $K10L2 $Ab10 
+liftoff -a 0.4 -s 0.4 -copies -sc 0.7 -flank 0.1 -g $OUTDIR/Pasa_Ab10/PASA_updates.3.trkinreg.gff3 -o $DIR/CI66_K10L2_v1.trkinreg.Ab10Liftoff.gff3 $K10L2 $Ab10 
 
 #Identify the liftedoff genes that didn't appear in the original annotations
-bedtools intersect -a $DIR/pasa_db_K10L2.gene_structures_post_PASA_updates.3815155.trkinreg.Ab10Liftoff.gff3 -b $DIR/pasa_db_K10L2.gene_structures_post_PASA_updates.3815155.trkinreg.gff3 -v > $DIR/CI66_K10L2_v1.trkinreg.Ab10liftoff.newonly.gff3
+bedtools intersect -a $DIR/CI66_K10L2_v1.trkinreg.Ab10Liftoff.gff3 -b OUTDIR/Pasa_K10L2/PASA_updates.2.gff3 -v > $DIR/CI66_K10L2_v1.trkinreg.Ab10liftoff.newonly.gff3
 
 #I downloaded this file in R and subset it to only the trkin specific regions. This script is under 08_FilterAndRename_Liftoff.R
+
 #This combines both gff3 files to create a new gff3 file with the new genes
-cat $DIR/CI66_K10L2_v1.trkinreg.Ab10liftoff.newonly.fix.gff3 /scratch/mjb51923/TRKIN_CRISPR/out_paper/Pasa_K10L2/pasa_db_K10L2.gene_structures_post_PASA_updates.3815155.gff3 > $DIR/CI66_K10L2_v1.gene.v2.gff3
+cat $DIR/CI66_K10L2_v1.trkinreg.Ab10liftoff.newonly.fix.gff3 $OUTDIR/Pasa_K10L2/PASA_updates.2.gff3 > $DIR/CI66_K10L2_v1.gene.gff3
 #This sorts the new gff3 file
-bedtools sort -i $DIR/CI66_K10L2_v1.gene.v2.gff3 > $DIR/CI66_K10L2_v1.gene.v2.sorted.gff3
+bedtools sort -i $DIR/CI66_K10L2_v1.gene.gff3 > $DIR/CI66_K10L2_v1.gene.sorted.gff3
