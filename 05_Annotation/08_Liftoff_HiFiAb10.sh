c712b8cb09710bcfa37b2e9c180a0bf2752165d5
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 19:12:23 -0400

    Update 08_Liftoff_HiFiAb10.sh

--- 05_Annotation/08_Liftoff_HiFiAb10.sh
+++ 05_Annotation/08_Liftoff_HiFiAb10.sh
@@ -30,6 +30,6 @@ bedtools intersect -a $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.gff3 -b $DIR/P
 #I downloaded this file in R and subset it to only the trkin specific regions These scripts are under 09_FilterAndRename_Liftoff.R
 
 #This combines both gff3 files to create a new gff3 file with the new genes
-cat $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.newonly.gff3 $OUTDIR/Pasa_Ab10/PASA_updates.2.gff3 > $DIR/B73_Ab10_HiFi_v2.gene.gff3
+cat $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.newonly.fix.gff3 $OUTDIR/Pasa_Ab10/PASA_updates.2.gff3 > $DIR/B73_Ab10_HiFi_v2.gene.gff3
 #This sorts the new gff3 file
 bedtools sort -i $$DIR/B73_Ab10_HiFi_v2.gene.gff3 > $DIR/B73_Ab10_HiFi_v2.gene.sorted.gff3
