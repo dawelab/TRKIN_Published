d92f8738831c928d0f1553010f131529f71a7d34
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 19:08:46 -0400

    Update 08_Liftoff_HiFiAb10.sh

--- 05_Annotation/08_Liftoff_HiFiAb10.sh
+++ 05_Annotation/08_Liftoff_HiFiAb10.sh
@@ -12,18 +12,17 @@ cd $DIR
 Ab10=/path/to/B73_Ab10_HiFi_v2.fa
 K10L2=/path/to/CI66_K10L2_v1.fasta
 
-#This subsets the K10L2 and Ab10 genomes to their respective trkin specific regions
+#This subsets the Ab10 genome to the trkin specific region (between the first two TR-1 knobs)
 samtools faidx $Ab10 chr10:142472000-153145000 > $DIR/B73_Ab10_HiFi_v2.trkinreg.fa
-samtools faidx $K10L2 K10L2:7429619-25502283 > $DIR/CI66_K10L2_v1.trkinreg.fa
 
-#I manually edited the K10L2 trkin region file to name the region K10L2 instead of K10L2:7429619-25502283 and the same for Ab10
+#I manually edited the Ab10 trkin region file to name the region Ab10 instead of chr10:142472000-153145000
 
 #This isolates the trkin region of each gene annotation file
 bedtools intersect -a $OUTDIR/Pasa_Ab10/PASA_updates.2.gff3 -b $DIR/B73_Ab10_HiFi_v2.trkinreg.fa -o $DIR/Pasa_Ab10/PASA_updates.3.trkinreg.gff3 
-bedtools intersect -a $OUTDIR/Pasa_K10L2/PASA_updates.2.gff3 -b DIR/CI66_K10L2_v1.trkinreg.fa -o $DIR/Pasa_K10L2/PASA_updates.3.trkinreg.gff3 
+ 
 
 #Lift over the annotations from K10L2 to the new Ab10 genome 
-liftoff -a 0.4 -s 0.4 -copies -sc 0.7 -flank 0.1 -g $DIR/Pasa_Ab10/PASA_updates.3.trkinreg.gff3 -o $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.gff3 $Ab10 $K10L2
+liftoff -a 0.4 -s 0.4 -copies -sc 0.7 -flank 0.1 -g $DIR/Pasa_K10L2/PASA_updates.3.trkinreg.gff3 -o $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.gff3 $Ab10 $K10L2
 
 #Identify the liftedoff genes that didn't appear in the original annotations
 bedtools intersect -a $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.gff3 -b $DIR/Pasa_Ab10/PASA_updates.3.trkinreg.gff3  -v > $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.newonly.gff3
