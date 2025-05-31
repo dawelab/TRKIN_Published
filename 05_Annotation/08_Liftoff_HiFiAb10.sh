3a09867a76d6ee893ce2c64a8859f6fb514a18d1
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 19:12:54 -0400

    Update 08_Liftoff_HiFiAb10.sh

--- 05_Annotation/08_Liftoff_HiFiAb10.sh
+++ 05_Annotation/08_Liftoff_HiFiAb10.sh
@@ -18,7 +18,7 @@ samtools faidx $Ab10 chr10:142472000-153145000 > $DIR/B73_Ab10_HiFi_v2.trkinreg.
 #I manually edited the Ab10 trkin region file to name the region Ab10 instead of chr10:142472000-153145000
 
 #This isolates the trkin region of each gene annotation file
-bedtools intersect -a $OUTDIR/Pasa_Ab10/PASA_updates.2.gff3 -b $DIR/B73_Ab10_HiFi_v2.trkinreg.fa -o $DIR/Pasa_Ab10/PASA_updates.3.trkinreg.gff3 
+bedtools intersect -a $OUTDIR/Pasa_Ab10/PASA_updates.3.gff3 -b $DIR/B73_Ab10_HiFi_v2.trkinreg.fa -o $DIR/Pasa_Ab10/PASA_updates.3.trkinreg.gff3 
  
 
 #Lift over the annotations from K10L2 to the new Ab10 genome 
@@ -30,6 +30,6 @@ bedtools intersect -a $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.gff3 -b $DIR/P
 #I downloaded this file in R and subset it to only the trkin specific regions These scripts are under 09_FilterAndRename_Liftoff.R
 
 #This combines both gff3 files to create a new gff3 file with the new genes
-cat $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.newonly.fix.gff3 $OUTDIR/Pasa_Ab10/PASA_updates.2.gff3 > $DIR/B73_Ab10_HiFi_v2.gene.gff3
+cat $DIR/B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.newonly.fix.gff3 $OUTDIR/Pasa_Ab10/PASA_updates.3.gff3 > $DIR/B73_Ab10_HiFi_v2.gene.gff3
 #This sorts the new gff3 file
 bedtools sort -i $$DIR/B73_Ab10_HiFi_v2.gene.gff3 > $DIR/B73_Ab10_HiFi_v2.gene.sorted.gff3
