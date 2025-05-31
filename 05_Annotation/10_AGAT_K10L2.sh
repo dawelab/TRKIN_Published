1eb4848337adbb02f0d72a37c63f550230e24b58
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 19:18:18 -0400

    Update 10_AGAT_K10L2.sh

--- 05_Annotation/10_AGAT_K10L2.sh
+++ 05_Annotation/10_AGAT_K10L2.sh
@@ -6,7 +6,7 @@ OUTDIR=""
 DIR=$OUTDIR/AGAT
 
 #Extract the CDS sequences
-agat_sp_extract_sequences.pl -g $OUTDIR/Liftoff/CI66_K10L2_v1.gene.v2.sorted.gff3 -f  $OUTDIR/RepeatMasker/CI66_K10L2_v1.fasta.masked -t cds -o $DIR/CI66_K10L2.v2.cds.fasta
+agat_sp_extract_sequences.pl -g $OUTDIR/Liftoff/CI66_K10L2_v1.gene.sorted.gff3 -f  $OUTDIR/RepeatMasker/CI66_K10L2_v1.fasta.masked -t cds -o $DIR/CI66_K10L2_v1.cds.fasta
 
 #Extract the mRNA sequences
-agat_sp_extract_sequences.pl -g $OUTDIR/Liftoff/CI66_K10L2_v1.gene.v2.sorted.gff3 -f  $OUTDIR/RepeatMasker/CI66_K10L2_v1.fasta.masked -t exon --merge -o $DIR/CI66_K10L2.v2.mRNA.fasta
+agat_sp_extract_sequences.pl -g $OUTDIR/Liftoff/CI66_K10L2_v1.gene.sorted.gff3 -f  $OUTDIR/RepeatMasker/CI66_K10L2_v1.fasta.masked -t exon --merge -o $DIR/CI66_K10L2_v1.mRNA.fasta
