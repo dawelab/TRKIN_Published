1068dc3d37d05b68f3ad8a3519a20f2f51c16291
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 19:14:30 -0400

    Update 09_FilterAndRename_Liftoff.R

--- 05_Annotation/09_FilterAndRename_Liftoff.R
+++ 05_Annotation/09_FilterAndRename_Liftoff.R
@@ -1,20 +1,28 @@
-Ab10_New <- read.delim("/Volumes/Transcend/Ab10_HiFi_v2_corrected.trkinreg.K10L2liftoff.newonly.gff3", header=FALSE, comment.char="#")
+
+setwd(")
+
+#Load in the new Ab10 annotations
+Ab10_New <- read.delim("B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.newonly.gff3", header=FALSE, comment.char="#")
 colnames(Ab10_New) <- c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute")
 
+#Isolate newly liftedover Ab10 annotations in the trkin region
 Ab10_New <- subset(Ab10_New, seqname == "chr10" & start >= 142472000 & end <= 153145000)
 
-#this renames the genes so that they don't overlab with existing annotations and I don't need to renumber all the genes
+#Rename the genes so that they don't overlab with existing annotations and are marked as liftedoff
 Ab10_New$attribute <- gsub("=g", "=gK", Ab10_New$attribute)
 
-write.table(Ab10_New, "/Volumes/Transcend/Ab10_HiFi_v2_corrected.trkinreg.K10L2liftoff.newonly.fix.gff3", sep = '\t', quote = FALSE, col.names = FALSE, row.names = FALSE)
+#Write out the updated file 
+write.table(Ab10_New, "B73_Ab10_HiFi_v2.trkinreg.K10L2liftoff.newonly.fix.gff3", sep = '\t', quote = FALSE, col.names = FALSE, row.names = FALSE)
 
-
-K10L2_New <- read.delim("/Volumes/Transcend/CI66_K10L2_v1.trkinreg.Ab10liftoff.newonly.gff3", header=FALSE, comment.char="#")
+#Load in the new K10L2 annotations
+K10L2_New <- read.delim("CI66_K10L2_v1.trkinreg.Ab10liftoff.newonly.gff3", header=FALSE, comment.char="#")
 colnames(K10L2_New) <- c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute")
 
+#Isolate newly liftedover K10L2 annotations in the trkin region
 K10L2_New <- subset(K10L2_New, seqname == "K10L2" & start >= 7429619 & end <= 25502283)
 
-#this renames the genes so that they don't overlab with existing annotations and I don't need to renumber all the genes
+#Rename the genes so that they don't overlab with existing annotations and are marked as liftedoff
 K10L2_New$attribute <- gsub("=g", "=gA", K10L2_New$attribute)
 
-write.table(K10L2_New, "/Volumes/Transcend/CI66_K10L2_v1.trkinreg.Ab10liftoff.newonly.fix.gff3", sep = '\t', quote = FALSE, col.names = FALSE, row.names = FALSE)
+#Write out the updated file 
+write.table(K10L2_New, "CI66_K10L2_v1.trkinreg.Ab10liftoff.newonly.fix.gff3", sep = '\t', quote = FALSE, col.names = FALSE, row.names = FALSE)
