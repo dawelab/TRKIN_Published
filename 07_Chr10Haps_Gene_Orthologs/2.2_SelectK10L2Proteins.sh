9b6416a787dfc3e0246db97a56dec97e8912d089
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 20:44:17 -0400

    Update 2.2_SelectK10L2Proteins.sh

--- 07_Chr10Haps_Gene_Orthologs/2.2_SelectK10L2Proteins.sh
+++ 07_Chr10Haps_Gene_Orthologs/2.2_SelectK10L2Proteins.sh
@@ -1,20 +1,13 @@
-#!/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Seqkit_K10L2Pro
-#SBATCH --output Seqkit_K10L2Pro.out
-#SBATCH --mem=50000
-#SBATCH --time=80:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=1
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=END
-
 module load seqtk/1.3-GCC-11.3.0
 module load SAMtools/1.11-GCC-10.2.0
 
-DIR="/scratch/mjb51923/TRKIN_CRISPR/out_paper/OrthoFinder"
-PROT="/scratch/mjb51923/TRKIN_CRISPR/out_paper/Entap/K10L2/entap_outfiles_nr_update/transcriptomes/CI66_K10L2_final.fasta"
+DIR=""
+#This file was generated in 05.12
+PROT="/path/to/Entap/K10L2/entap_outfiles_nr_update/transcriptomes/CI66_K10L2_final.fasta"
 
+#Select the protein sequences associated with genes on the non-shared region of the K10L2 haplotype
+
+#This loop produces a list of all the protein names
 while read i;
 do
 NAME=$i"\."
@@ -25,7 +18,9 @@ do
 done
 done < $DIR/CI66_K10L2.K10L2hapGeneNames.txt
 
+#This actually selects those fasta sequences 
 seqtk subseq $PROT $DIR/K10L2GeneNamesSpec.txt > $DIR/CI66_K10L2.K10L2hapProtein.fasta
+#This indexes the protein sequence fasta
 samtools faidx $DIR/CI66_K10L2.K10L2hapProtein.fasta
 
 
