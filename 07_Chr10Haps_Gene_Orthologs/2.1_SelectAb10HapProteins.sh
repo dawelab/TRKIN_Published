45f0b08fb0d3f95015ad9c94c92478cfd2e1268f
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 20:42:46 -0400

    Update 2.1_SelectAb10HapProteins.sh

--- 07_Chr10Haps_Gene_Orthologs/2.1_SelectAb10HapProteins.sh
+++ 07_Chr10Haps_Gene_Orthologs/2.1_SelectAb10HapProteins.sh
@@ -1,20 +1,13 @@
-#!/bin/bash
-#SBATCH --partition=batch
-#SBATCH -J Seqkit_Ab10HapPro
-#SBATCH --output Seqkit_Ab10HapPro.out
-#SBATCH --mem=50000
-#SBATCH --time=80:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=1
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=END
-
 module load seqtk/1.3-GCC-11.3.0
 module load SAMtools/1.11-GCC-10.2.0
 
-DIR="/scratch/mjb51923/TRKIN_CRISPR/out_paper/OrthoFinder"
-PROT="/scratch/mjb51923/TRKIN_CRISPR/out_paper/Entap/Ab10/entap_outfiles_v2/transcriptomes/HiFiAb10_final.fasta"
+DIR=""
+#This file was generated in 05.12
+PROT="path/to/Entap/Ab10/entap_outfiles_v2/transcriptomes/HiFiAb10_final.fasta"
 
+#Select the protein sequences associated with genes on the non-shared region of the Ab10 haplotype
+
+#This loop produces a list of all the protein names
 while read i
 do
 NAME=$i"\."
@@ -25,7 +18,9 @@ do
 done
 done < $DIR/HiFiAb10.Ab10hapGeneNames.txt
 
+#This actually selects those fasta sequences 
 seqtk subseq $PROT $DIR/Ab10hapGeneNamesSpec.txt > $DIR/HiFiAb10.Ab10hapProtein.fasta
+#This indexes the protein sequence fasta
 samtools faidx $DIR/HiFiAb10.Ab10hapProtein.fasta
 
 
