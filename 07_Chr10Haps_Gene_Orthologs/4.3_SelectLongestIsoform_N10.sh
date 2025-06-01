fdd7a6504a0ab355c1604cccff04f5f62cd79e1d
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 20:54:48 -0400

    Update 4.3_SelectLongestIsoform_N10.sh

--- 07_Chr10Haps_Gene_Orthologs/4.3_SelectLongestIsoform_N10.sh
+++ 07_Chr10Haps_Gene_Orthologs/4.3_SelectLongestIsoform_N10.sh
@@ -1,7 +1,10 @@
 module load seqtk/1.3-GCC-11.3.0
 
-DIR="/scratch/mjb51923/TRKIN_CRISPR/out_paper/OrthoFinder"
-PROT="/scratch/mjb51923/annotations/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.protein.fa"
+DIR=""
+#This file is assocaited with 
+#This file is from https://www.maizegdb.org/genome/assembly/Zm-B73-REFERENCE-NAM-5.0
+PROT="Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.protein.fa"
 
+#This selects the fasta sequence for the longest isoforms identified in the previous step
 seqtk subseq $PROT $DIR/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.protei.GeneNamesLongest.txt> $DIR/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.Protein.LongestIsoform.fasta
 
