bcda0c3d0e8795f9b08ff033b696627dba3c6db5
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-01 17:18:42 -0400

    Update 2_HISAT2_K10L2.sh

--- 09_Ab10_K10L2_trkin_Expression/2_HISAT2_K10L2.sh
+++ 09_Ab10_K10L2_trkin_Expression/2_HISAT2_K10L2.sh
@@ -1,26 +1,17 @@
-#!/usr/bin/bash
-#SBATCH --partition=highmem_p 
-#SBATCH -J HISAT2_K10L2
-#SBATCH --output HISAT2_K10L2.out
-#SBATCH --mem=500GB
-#SBATCH --time=48:00:00
-#SBATCH	--nodes=1
-#SBATCH	--ntasks=19
-#SBATCH --mail-user=meghan.brady@uga.edu
-#SBATCH --mail-type=BEGIN,END
-
 #load modules
 module load HISAT2/2.1.0-foss-2019b
 module load SAMtools/1.9-GCC-8.3.0
 
-#Define Variables
-READ_DIR="/scratch/mjb51923/TRKIN_CRISPR/out/K10L2_Trimmed"
-OUT_DIR="/scratch/mjb51923/TRKIN_CRISPR/out/RNA_Aln"
-REF="/scratch/mjb51923/ref_genomes/Zm-B73_AB10-REFERENCE-NAM-1.0.fa"
+#From doi 10.1101/gad.340679.120
+RNA_DIR="/path/to/reads"
+OUT_DIR=""
+#From doi 10.1186/s13059-020-02029-9
+REF="Zm-B73_AB10-REFERENCE-NAM-1.0.fa"
 
 #Index the reference
 hisat2-build $REF $REF
 
+#Align the K10L2 and N10 sequences to the reference, sort and index the bam files
 for i in K10L2_1 \
 K10L2_2 \
 K10L2_3 \
