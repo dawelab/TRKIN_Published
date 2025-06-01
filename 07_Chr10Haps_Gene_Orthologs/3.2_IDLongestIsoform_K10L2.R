a10880d5ba3b60c3f896a4f2e9ecbc722a24ccf1
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 20:49:00 -0400

    Update 3.2_IDLongestIsoform_K10L2.R

--- 07_Chr10Haps_Gene_Orthologs/3.2_IDLongestIsoform_K10L2.R
+++ 07_Chr10Haps_Gene_Orthologs/3.2_IDLongestIsoform_K10L2.R
@@ -3,20 +3,18 @@
 library(tidyr)
 library(dplyr)
 
-setwd("/scratch/mjb51923/TRKIN_CRISPR/out_paper/OrthoFinder")
+setwd("")
 
+#This file comes from indexing the protein fasta in 07.2.2
 K10L2HapProt <- read.delim("CI66_K10L2.K10L2hapProtein.fasta.fai", header=FALSE)
-
 names(K10L2HapProt) <- c("name", "length", "offset", "linebases", "linewidth")
 
-
+#This separates out the name field
 K10L2HapProt <- separate(K10L2HapProt, col = name, into=c("gene", "isoform"), sep="\\.", extra = "merge")
 
-genes <- unique(K10L2HapProt$gene)
-
+#This identifies the longest isoform for each gene
 Long <- K10L2HapProt %>% group_by(K10L2HapProt$gene) %>% arrange(-length) %>% slice(1)
 length(Long$gene)
-
 Long$ID <- paste(Long$gene, Long$isoform, sep=".")
 
 #Print a list of gene names and numbers of the longest isoforms
