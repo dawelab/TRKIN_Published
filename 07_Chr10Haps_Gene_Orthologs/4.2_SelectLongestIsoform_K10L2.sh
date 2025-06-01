4358ee999df03ac56e007581d199a9eb6dd856d0
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 20:52:55 -0400

    Update 4.2_SelectLongestIsoform_K10L2.sh

--- 07_Chr10Haps_Gene_Orthologs/4.2_SelectLongestIsoform_K10L2.sh
+++ 07_Chr10Haps_Gene_Orthologs/4.2_SelectLongestIsoform_K10L2.sh
@@ -1,7 +1,9 @@
 module load seqtk/1.3-GCC-11.3.0
 
-DIR="/scratch/mjb51923/TRKIN_CRISPR/out_paper/OrthoFinder"
-PROT="/scratch/mjb51923/TRKIN_CRISPR/out_paper/Entap/K10L2/entap_outfiles_nr_update/transcriptomes/CI66_K10L2_final.fasta"
+DIR=""
+#This file was generated in 05.12
+PROT="/path/to/Entap/K10L2/entap_outfiles_nr_update/transcriptomes/CI66_K10L2_final.fasta"
 
+#This selects the fasta sequence for the longest isoforms identified in the previous step
 seqtk subseq $PROT $DIR/CI66_K10L2.K10L2hapProtein.fasta.K10L2hapGeneNamesLongest.txt > $DIR/CI66_K10L2.K10L2hapProtein.LongestIsoform.fasta
 
