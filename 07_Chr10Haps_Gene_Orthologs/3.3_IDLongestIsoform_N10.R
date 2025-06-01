8a687830d20f7f7bec37066e5fb1fd2bb9bb9589
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-05-31 20:50:27 -0400

    Update 3.3_IDLongestIsoform_N10.R

--- 07_Chr10Haps_Gene_Orthologs/3.3_IDLongestIsoform_N10.R
+++ 07_Chr10Haps_Gene_Orthologs/3.3_IDLongestIsoform_N10.R
@@ -3,20 +3,17 @@
 library(tidyr)
 library(dplyr)
 
-setwd("/scratch/mjb51923/TRKIN_CRISPR/out_paper/OrthoFinder")
+setwd("")
 
 #This file is from https://www.maizegdb.org/genome/assembly/Zm-B73-REFERENCE-NAM-5.0
-B73Prot <- read.delim("/scratch/mjb51923/annotations/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.protein.fa.fai", header=FALSE)
-
+B73Prot <- read.delim("Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.protein.fa.fai", header=FALSE)
 names(B73Prot) <- c("name", "length", "offset", "linebases", "linewidth")
 
+#This separates out the name field
 B73Prot <- separate(B73Prot, col = name, into=c("gene", "isoform"), sep="_")
 
-genes <- unique(B73Prot$gene)
-
+#This isolates the longest isoform for each gene
 Long <- B73Prot %>% group_by(B73Prot$gene) %>% arrange(-length) %>% slice(1)
-length(Long$gene)
-
 Long$ID <- paste(Long$gene, Long$isoform, sep="_")
 
 #Print a list of gene names and numbers of the longest isoforms
