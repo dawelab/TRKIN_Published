cf97eb89d2d4e7e42206f49f17f96f37362f2e0d
Meghan Brady <58435529+mjb51923@users.noreply.github.com>
2025-06-06 19:24:59 -0400

    Update 2_trkin_Fitness_Mapping.R

--- 13_Homozygous_Ab10_trkin_Mutants_Fitness/2_trkin_Fitness_Mapping.R
+++ 13_Homozygous_Ab10_trkin_Mutants_Fitness/2_trkin_Fitness_Mapping.R
@@ -2,9 +2,9 @@ library(readxl)
 library(ggplot2)
 library(gvlma)
 
-setwd("/Users/user/University_of_Georgia/Dawe_Lab_Documents/Trkin_CRISPR/R\ Sessions/Paper/TRKIN_Published/Homozygous_Fitness")
+setwd("")
 
-DATA <- read_excel("~/University_of_Georgia/Dawe_Lab_Documents/Trkin_CRISPR/Trkin_MappingPopulation_v2.xlsx")
+DATA <- read_excel("Trkin_MappingPopulation_v2.xlsx")
 DATA$`Height (cm)` <- as.numeric(DATA$`Height (cm)`)
 DATA$`Height_Bin` <- as.factor(DATA$`Height_Bin`)
 DATA$Pot <- as.factor(DATA$Pot)
